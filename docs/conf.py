182238aa4b95ed07d0d150124b12d14e063219e6
nikkjonee <nikkjone05@gmail.com>
2025-06-20 13:12:12 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@ exclude_patterns = []
 html_theme = 'alabaster'
 html_static_path = ['_static']
 
-    html_favicon = '_static/totaldefense.png'
+html_favicon = '_static/totaldefense.png'
 
 html_js_files = [
     'livechat.js',
